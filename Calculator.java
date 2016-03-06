f9657e205269a935573ab2822d6bed4f15520d72
islam123786 <islam123786@gmail.com>
2016-03-06 23:48:45 +0530

    First Commit

---
+++ Calculator.java
@@ -0,0 +1,38 @@
+package Calulator;
+
+
+public class Calculator {
+	
+	
+	static int Add(int val1, int val2)
+	{
+		return val1 + val2;
+	}
+	
+	static int Substration(int val1, int val2)
+	{
+		return val1 - val2;
+	}
+	
+	static int Multiply(int val1, int val2)
+	{
+		return val1*val2;
+	}
+
+	public static void main(String[] args) {
+		
+		int val1 = 10;
+		int val2 = 20;
+		
+		int AddResult = Add(val1, val2);
+		int SubResult = Substration(val2, val1);
+		int MulResult = Multiply(val1, val2);
+		
+		int x =30;
+		
+
+	}
+	
+	
+
+}
